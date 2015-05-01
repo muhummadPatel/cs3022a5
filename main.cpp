43626cc149616cac7e7ad7a2bccb69da3ee289d1
Muhummad Patel <muhummad.patel@gmail.com>
2015-05-01 15:36:13 +0200

    Add main class.

---
+++ main.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main()
+{
+    cout << "Hello World!" << endl;
+    std::string abc = "hello";
+    //char * arr = new char[800];
+    return 0;
+}
+
