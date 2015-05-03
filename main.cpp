2ed1a76a5be1a73dd4f629266f31b798bcd01aab
Muhummad Patel <muhummad.patel@gmail.com>
2015-05-03 20:04:21 +0200

    Simple test operations added to main method.

--- main.cpp
+++ main.cpp
@@ -1,13 +1,21 @@
+#include <cstdint>
 #include <iostream>
 #include <string>
 
-using namespace std;
+#include <audio.h>
+
+using namespace ptlmuh006;
 
 int main()
 {
-    cout << "Hello World!" << endl;
-    std::string abc = "hello";
-    //char * arr = new char[800];
-    return 0;
-}
+    std::cout << "Running main" << std::endl;
 
+    Audio<int16_t> a(44100, 16, 1);
+    a.read("frogs18sec_44100_signed_16bit_mono.raw");
+
+    Audio<int16_t> b(44100, 16, 1);
+    b.read("frogs18sec_44100_signed_16bit_mono.raw");
+
+    Audio<int16_t> c = a | b;
+    c.save("aCATb.raw");
+}
