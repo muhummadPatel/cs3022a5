7f50d65c6df262aabcc360f3660b1bd337d84804
Muhummad Patel <muhummad.patel@gmail.com>
2015-05-06 12:43:58 +0200

    Add required files for unit testing.

---
+++ test.cpp
@@ -0,0 +1,15 @@
+#define CATCH_CONFIG_MAIN
+#include "catch.hpp"
+
+#include "audio.h"
+
+using namespace ptlmuh006;
+
+TEST_CASE("Test case description", "[tag]"){
+
+    SECTION("Test case section description"){
+        //test conditions
+        REQUIRE( 0 == 1 );
+    }
+
+}
