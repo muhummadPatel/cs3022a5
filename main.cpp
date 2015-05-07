f004a262675d2b9002086a157f5ac0781b6f51e0
Muhummad Patel <muhummad.patel@gmail.com>
2015-05-07 13:30:00 +0200

    Load and save stereo file. (Quick and dirty 'test')

--- main.cpp
+++ main.cpp
@@ -10,28 +10,9 @@ int main()
 {
     std::cout << "Running main" << std::endl;
 
-    Audio<int16_t> a(44100, 16, 1);
-    a.read("frogs18sec_44100_signed_16bit_mono.raw");
-
-    Audio<int16_t> b(44100, 16, 1);
-    b.read("frogs18sec_44100_signed_16bit_mono.raw");
-
-    Audio<int16_t> c = a | b;
-    c.save("aCATb.raw");
-
-    std::pair<float, float> factor(0.5f, 0.0f);
-    Audio<int16_t> f = a * factor;
-    f.save("aFACThalf.raw");
-    
-    Audio<int16_t> x(44100, 16, 1);
-    x.read("siren40sec_44100_signed_16bit_mono.raw");
-    
-    Audio<int16_t> p = a + x;
-    p.save("aPLUSx.raw");
-    
-    std::pair<int, int> range(44100, 44100*5);
-    Audio<int16_t> y = a ^ range;
-    y.save("aCUT4secs.raw");
+    Audio<std::pair<int16_t, int16_t>> a(44100, 16, 2);
+    a.read("frogs18sec_44100_signed_16bit_stereo.raw");
+    a.save("STEEEERRRRREEOOOO.raw");
     
 //    for(auto it = y.begin(); it != y.end(); ++it){
 //        std::cout << *it << std::endl;
