1bacfcf11100dd93af388566b68846bc75229539
Muhummad Patel <muhummad.patel@gmail.com>
2015-05-07 22:34:03 +0200

    Add unit tests for mono audio transformations.

--- test.cpp
+++ test.cpp
@@ -41,7 +41,7 @@ TEST_CASE("Test Mono Operator Overloads", ""){
         REQUIRE( factored.getData().size() == expected.size() );
         REQUIRE( factored.getData() == expected );
     }
-    //32767
+
     SECTION("Test addition operator a+b"){
         //GIVEN: some audio objects a and b
         std::vector<int16_t> samplesB = {-1, 1, 2, -2, 3, -3, std::numeric_limits<int16_t>::max(), std::numeric_limits<int16_t>::min()};
@@ -69,6 +69,65 @@ TEST_CASE("Test Mono Operator Overloads", ""){
         REQUIRE( cut.getData() == expected );
     }
 }
+
+TEST_CASE("Test Mono Audio Transformations", ""){
+
+    int sampleRate = 44100,
+        bitCount = 16,
+        channels = 1;
+
+    std::vector<int16_t> samplesA = {-1, 1, 2, -2, 3, -3, 10, -10};
+    Audio<int16_t> a(sampleRate, bitCount, channels, samplesA);
+
+    SECTION("Test reverse transformation"){
+        //GIVEN: some audio file a
+        //WHEN: we apply the reverse transformation to a
+        Audio<int16_t> rev = a.reverse();
+
+        //THEN: we should get a new audio object with all the samples reversed
+        std::vector<int16_t> expected = {-10, 10, -3, 3, -2, 2, 1, -1};
+        REQUIRE( rev.getData().size() == expected.size() );
+        REQUIRE( rev.getData() == expected );
+    }
+
+    SECTION("Test ranged add transformation"){
+        //GIVEN: some audio files a and b
+        std::vector<int16_t> samplesB = {-1, 1, std::numeric_limits<int16_t>::max(), 2, std::numeric_limits<int16_t>::min(), -3, 10, -10};
+        Audio<int16_t> b(sampleRate, bitCount, channels, samplesB);
+
+        //WHEN: we apply the ranged add transformation to a and b
+        Audio<int16_t> radd = Audio<int16_t>::rangedAdd(a, std::pair<int, int>(1, 4), b, std::pair<int, int>(2, 5));
+
+        //THEN: we should get a new audio object with all the samples in the given ranges added
+        std::vector<int16_t> expected = {0, std::numeric_limits<int16_t>::max(), 4, std::numeric_limits<int16_t>::min()};
+        REQUIRE( radd.getData().size() == expected.size() );
+        REQUIRE( radd.getData() == expected );
+        REQUIRE( a.getData().size() == samplesA.size() );
+        REQUIRE( b.getData().size() == samplesB.size() );
+    }
+
+    SECTION("Test compute rms transformation"){
+        //GIVEN: some audio object a
+        //WHEN: we compute the rms for that audio object
+        float rms = a.computeRMS();
+
+        //THEN: we should get the correct RMS value and the object should not have changed
+        float expected = 5.338539126;
+        REQUIRE( rms == Approx(expected) );
+        REQUIRE( a.getData() == samplesA);
+    }
+
+    SECTION("Test sound nnormalization transformation"){
+        //GIVEN: some audio object a
+        //WHEN: we normalize to a specific rms value
+        std::vector<int16_t> samplesB = {1, 1, 1, 2, 1, 1, 1, 1, 1, 1};
+        Audio<int16_t> b(sampleRate, bitCount, channels, samplesB);
+        Audio<int16_t> norm = b.normalized(std::pair<float, float>(20.0f, 20.0f));
+
+        //THEN: we should get a new audio object with the channel normalized to the specified level
+        REQUIRE( norm.computeRMS() == Approx(20).epsilon(1.0));
+        REQUIRE( a.getData() == samplesA);
+    }
 }
 
 TEST_CASE("Test Mono Move/Copy Semantics", ""){
