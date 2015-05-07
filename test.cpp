6b66fb98d4e52d4da8f1ee5c21a34b58d2d44594
Muhummad Patel <muhummad.patel@gmail.com>
2015-05-07 13:30:37 +0200

    Add unit tests to check mono audio operator overloads and copy/move semantics.

--- test.cpp
+++ test.cpp
@@ -1,15 +1,113 @@
 #define CATCH_CONFIG_MAIN
 #include "catch.hpp"
 
+#include <cstdint>
+#include <vector>
+
 #include "audio.h"
 
 using namespace ptlmuh006;
 
-TEST_CASE("Test case description", "[tag]"){
+TEST_CASE("Test Mono Operator Overloads", ""){
+    
+    int sampleRate = 44100,
+        bitCount = 16,
+        channels = 1;
+        
+    std::vector<int16_t> samplesA = {-1, 1, 2, -2, 3, -3, 10, -10};
+    Audio<int16_t> a(sampleRate, bitCount, channels, samplesA);
+    
+    SECTION("Test concatenate operator a|b"){
+        //GIVEN: some audio objects a and b
+        std::vector<int16_t> samplesB = {-1, 1, 2, -2, 3, -3, 10, -10};
+        Audio<int16_t> b(sampleRate, bitCount, channels, samplesB);
+        
+        //WHEN: we concatenate them using the concatenation operator
+        Audio<int16_t> concatenated = a | b;
+        
+        //THEN: the result should be the two files back to back
+        std::vector<int16_t> expected = {-1, 1, 2, -2, 3, -3, 10, -10, -1, 1, 2, -2, 3, -3, 10, -10};
+        REQUIRE( concatenated.getData().size() == expected.size() );
+        REQUIRE( concatenated.getData() == expected );
+    }
+    
+    SECTION("Test volume factor operator a*f"){
+        //GIVEN: some audio object a        
+        //WHEN: we apply the volume factor operator with 0.5
+        Audio<int16_t> factored = a * std::pair<float, float>(0.5f, 0.5f);
+        
+        //THEN: the result should have all samples halved and clamped
+        std::vector<int16_t> expected = {0, 0, 1, -1, 1, -1, 5, -5};
+        REQUIRE( factored.getData().size() == expected.size() );
+        REQUIRE( factored.getData() == expected );
+    }
+    //32767
+    SECTION("Test addition operator a+b"){
+        //GIVEN: some audio objects a and b
+        std::vector<int16_t> samplesB = {-1, 1, 2, -2, 3, -3, std::numeric_limits<int16_t>::max(), std::numeric_limits<int16_t>::min()};
+        Audio<int16_t> b(sampleRate, bitCount, channels, samplesB);
+        
+        //WHEN: we add a and b
+        Audio<int16_t> sum = a + b;
+        
+        //THEN: the result should have all samples added together and clamped
+        std::vector<int16_t> expected = {-2, 2, 4, -4, 6, -6, std::numeric_limits<int16_t>::max(), std::numeric_limits<int16_t>::min()};
+        //int16_t yo = 32767 + 5;
+        //REQUIRE( yo == 0 );
+        REQUIRE( sum.getData().size() == expected.size() );
+        REQUIRE( sum.getData() == expected );
+    }
+}
 
-    SECTION("Test case section description"){
-        //test conditions
-        REQUIRE( 0 == 1 );
+TEST_CASE("Test Mono Move/Copy Semantics", ""){
+    
+    int sampleRate = 44100,
+        bitCount = 16,
+        channels = 1;
+    std::vector<int16_t> samples = {-1, 1, 2, -2, 3, -3, 10, -10};
+    
+    Audio<int16_t> aud(sampleRate, bitCount, channels, samples);
+    
+    SECTION("Test move constructor"){
+        //GIVEN: A constructed object (aud)
+        //WHEN: We create a new object using the move constructor
+        Audio<int16_t> newAud = std::move(aud);
+        
+        //THEN: aud should be empty and newAud should be constructed
+        REQUIRE( aud.getData().size() == 0 );
+        REQUIRE( newAud.getData() == samples );
+    }
+    
+    SECTION("Test copy constructor"){
+        //GIVEN: A constructed object (aud)
+        //WHEN: We create a new object using the copy constructor
+        Audio<int16_t> newAud = aud;
+        
+        //THEN: aud should be unchanged and newAud should be constructed
+        REQUIRE( aud.getData() == samples );
+        REQUIRE( newAud.getData() == samples );
+    }
+    
+    SECTION("Test copy assignment"){
+        //GIVEN: A constructed object (aud)
+        //WHEN: We copy it into another object
+        Audio<int16_t> newAud;
+        newAud = aud;
+        
+        //THEN: aud should be unchanged and newAud should have a copy of aud
+        REQUIRE( aud.getData() == samples );
+        REQUIRE( newAud.getData() == samples );
+    }
+    
+    SECTION("Test move assignment"){
+        //GIVEN: A constructed object (aud)
+        //WHEN: We move it into another object
+        Audio<int16_t> newAud;
+        newAud = std::move(aud);
+        
+        //THEN: aud should be empty and newAud should have a copy of aud
+        REQUIRE( aud.getData().size() == 0 );
+        REQUIRE( newAud.getData() == samples );
     }
 
 }
