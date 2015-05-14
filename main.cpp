d93a83b9b169ec460a551ff65673c983e1238b43
Muhummad Patel <muhummad.patel@gmail.com>
2015-05-14 16:33:40 +0200

    WIP: Start implementing command line argument parsing. Done for the add option.

--- main.cpp
+++ main.cpp
@@ -4,17 +4,96 @@
 
 #include "audio.h"
 
-using namespace ptlmuh006;
+//TODO: FIX RANGED ADD TO USE SECONDS
 
-int main()
-{
-    std::cout << "Running main" << std::endl;
-
-    Audio<std::pair<int16_t, int16_t>> a(44100, 16, 2);
-    a.read("frogs18sec_44100_signed_16bit_stereo.raw");
-    a.save("STEEEERRRRREEOOOO.raw");
+void printUsage(){
+    using namespace std;
     
-//    for(auto it = y.begin(); it != y.end(); ++it){
-//        std::cout << *it << std::endl;
-//    }
+    cout << "\nUsage:" << endl;
+    cout << "./samp -r sampleRateInHz -b bitcount -c noChannels" << endl;
+    cout << "\t [-o outfilename] [<ops>]" << endl;
+    cout << "\t sndfile1 [sndfile2]" << endl;
+    cout << "\nOptions:___" << endl;
+    cout << "\t-add: add sndFile1 and sndFile2" << endl;
+    cout << "\t-cut r1 r2: remove samples over range [r1,r2] (inclusive)" << endl;
+    cout << "\t-radd r1 r2 s1 s2 : add sndFile1 and sndFile2 over sub-ranges(seconds)" << endl;
+    cout << "\t-cat: concatenate soundFile1 and soundFile2" << endl;
+    cout << "\t-v r1 r2: volume factor for left/right audio" << endl;
+    cout << "\t-rev: reverse sound file" << endl;
+    cout << "\t-rms: Prints out the RMS of the soundfile" << endl;
+    cout << "\t-norm r1 r2: normalize file for left/right audio" << endl;
+    cout << "\n"<< endl;
+}
+
+template <typename T> int parseOptions(int sampleRate, int bitCount, int noChannels, int argc, char* argv[], std::string outFilename){
+    using namespace ptlmuh006;    
+    
+    int pos = (outFilename == "out.raw")? 7 : 9;
+    std::cout << "pos" << pos << std::endl;
+    
+    if(std::string(argv[pos]) == "-add"){
+        if(argc != (pos+2)+1) return 1;
+        
+        Audio<T> aud1(argv[pos+1], sampleRate, bitCount, noChannels);
+        Audio<T> aud2(argv[pos+2], sampleRate, bitCount, noChannels);
+        Audio<T> sum = aud1 + aud2;
+        sum.save(outFilename);
+        
+        return 0;
+    }else if(std::string(argv[pos]) == "-cut"){
+        return 1;
+    }
+}
+
+int main(int argc, char* argv[]){
+    using namespace ptlmuh006;
+    
+    if(argc < 8){
+        printUsage();
+        return 1;
+    }else{
+        int sampleRate = std::stoi(argv[2]);
+        int bitCount = std::stoi(argv[4]);
+        int noChannels = std::stoi(argv[6]);
+        
+        if(bitCount != 8 && bitCount != 16){
+            std::cout << "Invalid bitCount (must be either 8 or 16)" << std::endl;
+            return 1;
+        }
+        
+        if(noChannels != 1 && noChannels != 2){
+            std::cout << "Invalid noChannels (must be either 1 or 2)" << std::endl;
+            return 1;
+        }
+        
+        std::string outFilename = "out.raw";
+        if(std::string(argv[7]) == "-o"){
+            outFilename = std::string(argv[8]);
+        }
+        
+        int response = 0;
+        std::cout << argc << std::endl;
+        
+        if(noChannels == 1){
+            if(bitCount == 8){
+                response = parseOptions<int8_t>(sampleRate, bitCount, noChannels, argc, argv, outFilename);
+            }else if(bitCount == 16){
+                response = parseOptions<int16_t>(sampleRate, bitCount, noChannels, argc, argv, outFilename);
+            }
+        }else if(noChannels == 2){
+            if(bitCount == 8){
+                response = parseOptions<std::pair<int8_t, int8_t>>(sampleRate, bitCount, noChannels, argc, argv, outFilename);
+            }else if(bitCount == 16){
+                response = parseOptions<std::pair<int16_t, int16_t>>(sampleRate, bitCount, noChannels, argc, argv, outFilename);
+            }
+        }
+        
+        if(response != 0){
+            std::cout << "ERRRORRR" << std::endl;
+        }
+        
+        std::cout << sampleRate << " " << bitCount << " " << noChannels << std::endl;
+    }
+    
+    return 0;
 }
